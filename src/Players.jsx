fe8b95c3df3168cf95db13d3c6d5ecd7ec0b8be8
vaisberger <yaelw123@gmail.com>
2024-05-25 23:20:21 +0300

    5.25 1

--- src/Players.jsx
+++ src/Players.jsx
@@ -1,47 +1,51 @@
 import React, { useState } from 'react';
 import { exit } from './login.jsx'
 function Players() {
-    const [pname, setName] = useState('');
-    const [password, setPassword] = useState('');
-    const [score, setScore] = useState(0);
-    const [players, setPlayers] = useState(() => {
-        // Retrieve users from local storage or initialize with an empty array
-        const savedPlayers = localStorage.getItem('players');
-        return savedPlayers ? JSON.parse(savedPlayers) : [];
-    });
-    const handleNameChange = (event) => {
-        setName(event.target.value);
+  const [pname, setName] = useState('');
+  const [password, setPassword] = useState('');
+  const [score, setScore] = useState(0);
+  const [players, setPlayers] = useState(() => {
+    // Retrieve users from local storage or initialize with an empty array
+    const savedPlayers = localStorage.getItem('players');
+    return savedPlayers ? JSON.parse(savedPlayers) : [];
+  });
+  const handleNameChange = (event) => {
+    setName(event.target.value);
+  }
+  const handlePasswordChange = (event) => {
+    setPassword(event.target.value);
+  }
+  const addTblRow = () => {
+    const row = document.createElement('tr');
+    row.innerHTML = '<p>Player: ' + pname + '<br></br>' + 'Score: ' + score + '<p>'
+    document.getElementById('tbl').appendChild(row);
+  }
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const Players = JSON.parse(localStorage.getItem('players'));
+    const index = Players.findIndex(u => u.name === pname);
+    if (index !== -1) {                                            /**לבדוק שעובד */
+      setScore(Players[index].score);
+    } else {
+      const newPlayer = {
+        id: players.length + 1,
+        pname,
+        password,
+        score
+      };
+      const updatedPlayers = [...players, newPlayer];
+      setPlayers(updatedPlayers);
+      localStorage.setItem('players', JSON.stringify(updatedPlayers));
     }
-    const handlePasswordChange = (event) => {
-        setPassword(event.target.value);
-    }
-    const addTblRow=()=>{
-      const row=document.createElement('tr');
-      row.innerHTML='<p>Player: ' +pname+ '<br></br>'+'Score: '+score+'<p>'
-      document.getElementById('tbl').appendChild(row);
-    }
-    const handleSubmit = (event) => {
-        event.preventDefault();
-    
-        const newPlayer = {
-            id: players.length + 1,
-            pname,
-            password,
-            score
-        };
-        const updatedPlayers = [...players, newPlayer];
-        setPlayers(updatedPlayers);
-        localStorage.setItem('players', JSON.stringify(updatedPlayers));
-    
-        // Clear the input fields
-        setName('');
-        setPassword('');
-        exit('newPlayer');
-        addTblRow();
-    }
-    return(
-      <div>
-        <div class="grid-container">
+    // Clear the input fields
+    setName('');
+    setPassword('');
+    exit('newPlayer');
+    addTblRow();
+  }
+  return (
+    <div>
+      <div class="grid-container">
         <div class="grid-item">
           <form id="newPlayer" onSubmit={handleSubmit}>
             <span className="exit" onClick={() => exit('newPlayer')}></span>
@@ -61,11 +65,11 @@ function Players() {
           </form>
         </div>
       </div>
-      <table id='tbl'> 
+      <table id='tbl'>
       </table>
-      </div>
-    )
-} 
+    </div>
+  )
+}
 export default Players
 
 
