920fec1ed1bd9dd8e7246b7fa13ae5f43a965b5f
vaisberger <yaelw123@gmail.com>
2024-05-29 00:46:33 +0300

    29.5 1

--- App2/Game.jsx
+++ App2/Game.jsx
@@ -6,23 +6,22 @@ export function Game() {
     const [CurrentPlayers, setCurrentPlayers] = useState([]);
     const [isGameOn, setIsGameOn] = useState(false);
     const [currentTurn, setCurrentTurn] = useState(0);
-    const [showWinners,setShowWinners]=useState(false);
+    const [showWinners, setShowWinners] = useState(false);
 
     function handleNewPlayerSubmit(e) {
         e.preventDefault();
         const playerName = e.target.querySelector('input[type="text"]').value;
-        const playerPassword = e.target.querySelector('input[type="password"]').value;
-        const playerExists = StoragedPlayers.find(player => player.pname === playerName && player.password === playerPassword);
-        const playerAlreadyInCurrent = CurrentPlayers.find(player => player.pname === playerName && player.password === playerPassword);
-        
+        const playerExists = StoragedPlayers.find(player => player.pname === playerName);
+        const playerAlreadyInCurrent = CurrentPlayers.find(player => player.pname === playerName);
+
         if (!playerAlreadyInCurrent) {
-            if (playerExists) 
+            if (playerExists)
                 setCurrentPlayers([...CurrentPlayers, playerExists]);
             else {
-                const newPlayer = { password: playerPassword, pname: playerName, scores: [] };
-                localStorage.setItem("players", JSON.stringify([...StoragedPlayers, newPlayer])); 
-                setStoragedPlayers([...StoragedPlayers, newPlayer]); 
-                setCurrentPlayers([...CurrentPlayers, newPlayer]); 
+                const newPlayer = { pname: playerName, scores: [] };
+                localStorage.setItem("players", JSON.stringify([...StoragedPlayers, newPlayer]));
+                setStoragedPlayers([...StoragedPlayers, newPlayer]);
+                setCurrentPlayers([...CurrentPlayers, newPlayer]);
             }
         } else {
             alert("Player is already in");
@@ -31,14 +30,14 @@ export function Game() {
 
     function handleGameEnd() {
         setIsGameOn(false);
-        setCurrentPlayers([]); 
+        setCurrentPlayers([]);
     }
 
     function handleNextTurn() {
-        setCurrentTurn(currentTurn=> (currentTurn + 1) % CurrentPlayers.length); 
+        setCurrentTurn(currentTurn => (currentTurn + 1) % CurrentPlayers.length);
     }
 
-    
+
 
     function calculateAverage(array) {
         if (array.length === 0) return 0;
@@ -50,67 +49,69 @@ export function Game() {
             ...player,
             averageScore: calculateAverage(player.scores)
         }));
-    
+
         playersWithAverage.sort((a, b) => a.averageScore - b.averageScore);
         return playersWithAverage.slice(0, 3);
     }
-    
+
 
     return (
         <>
             <div className={classes.gameContainer}>
-            <h1 className={classes.title}>Get to 100</h1>
-            <button className={classes.gameButton} onClick={() => setShowWinners(!showWinners)}>show winners</button>
-            {showWinners ? (
-                <>
-                    <h1>The top Players Are:</h1>
-                    <ol>
-                        {topPlayers(StoragedPlayers).map((player, index) => (
-                            <li key={index}>{player.pname} - Average Score: {player.averageScore}</li>
-                        ))}
-                    </ol>
-                </>
-            ) : (
-                <>
-                    {!isGameOn && (
-                        <form onSubmit={handleNewPlayerSubmit}>
-                            <input
-                                type="text"
-                                placeholder="Enter player's name"
-                                required
-                            />
-                            <input
-                                type="password"
-                                placeholder="Enter your password"
-                                required
-                            />
-                            <button className={classes.gameButton} type="submit">Add Player</button>
-                        </form>
-                    )}
-                    {!isGameOn && CurrentPlayers.length > 0 && (
-                        <div>
-                            {CurrentPlayers.map((player, index) => (
-                                <div key={index}>
-                                    <p>Name: {player.pname}, Scores: {player.scores}</p>
+                <h1 className={classes.title}>Get to 100</h1>
+                <button className={classes.gameButton} onClick={() => setShowWinners(!showWinners)}>show winners</button>
+                <button className={classes.gameButton} onClick={() => { setIsGameOn(!isGameOn) }}>start game</button>
+                {showWinners ? (
+                    <>
+                        <h1>The top Players Are:</h1>
+                        <ol>
+                            {topPlayers(StoragedPlayers).map((player, index) => (
+                                <li key={index}>{player.pname} - Average Score: {player.averageScore}</li>
+                            ))}
+                        </ol>
+                    </>
+                ) : (
+                    <>
+                        {!isGameOn && (
+                            <div className={classes.container}>
+                                <div className={classes.item}>
+                                    <form className={classes.newPlayer} onSubmit={handleNewPlayerSubmit}>
+                                        <input
+                                            type="text"
+                                            placeholder="Enter player's name"
+                                            required
+                                        />
+                                        <div className={classes.item}>
+                                            <button className={classes.gameButton} type="submit">Add Player</button>
+                                        </div>
+                                    </form>
                                 </div>
-                            ))}
-                        </div>
-                    )}
-                    <button className={classes.gameButton} onClick={()=>{setIsGameOn(!isGameOn)}}>start game</button>
-                    {isGameOn && (
-                        <>
-                            {CurrentPlayers.map((player, index) => (
-                                <Board player={player} endGame={handleGameEnd} isMyTurn={currentTurn === index} moveTurn={handleNextTurn} />
-                            ))}
-                        </>
-                    )}
-                </>
-            )}
+                            </div>
+                        )}
+                        {!isGameOn && CurrentPlayers.length > 0 && (
+                            <div>
+                                {CurrentPlayers.map((player, index) => (
+                                    <div key={index}>
+                                        <p>Name: {player.pname}, Scores: {player.scores}</p>
+                                    </div>
+                                ))}
+                            </div>
+                        )}
+
+                        {isGameOn && (
+                            <>
+                                {CurrentPlayers.map((player, index) => (
+                                    <Board player={player} endGame={handleGameEnd} isMyTurn={currentTurn === index} moveTurn={handleNextTurn} />
+                                ))}
+                            </>
+                        )}
+                    </>
+                )}
             </div>
         </>
     );
-    
-    
+
+
 }
 
 export default Game;
