b5ca78f931280c7ea673b49481e651b039c13189
moriel2003 <morderi2142@gmail.com>
2024-05-30 08:06:32 +0300

    updated text editor to handle color size and font changes

--- App1/jsx/KeyBoard.jsx
+++ App1/jsx/KeyBoard.jsx
@@ -12,9 +12,36 @@ function KeyBoard(props){
     } else if (props.currentLanguage === 'Chinese') {
         currentLanguage = Chinese;
     }
-  
+
+    const handleColorChange = (event) => {
+        props.changeColorTo(event.target.value); 
+    };
+    const handleFontChange = (event) => {
+        props.changeFontTo(event.target.value);
+    };
+
+    const handleSizeChange = (event) => {
+        props.changeSizeTo(parseInt(event.target.value));
+    };
+    const handleTextCaseChange = (selectedCase) => {
+        props.changeCaseTo(selectedCase);
+    };
+
     return (
+        
         <div className={classes.keyboardContainer}>
+            <button onClick={props.clearText}>clear</button>
+                <div className={classes.keyboardContainer}>
+            <input type="color" onChange={handleColorChange} />
+            <select onChange={handleFontChange}>
+                <option value="Arial">Arial</option>
+                <option value="Times New Roman">Times New Roman</option>
+           
+            </select>
+            <input type="number" min="8" max="72" defaultValue="16" onChange={handleSizeChange} />
+            <button onClick={() => handleTextCaseChange('upper')}>UPPER CASE</button>
+            <button onClick={() => handleTextCaseChange('lower')}>lower case</button>
+        </div>
         <div className={classes.keyboard}>
             {currentLanguage.map((letter, index) => (
                 <button key={index} onClick={() => props.onTextChanges(letter)}>{letter}</button>
@@ -22,6 +49,7 @@ function KeyBoard(props){
         <button className={classes.spaceButton} onClick={() => props.onTextChanges(' ')} >Space</button>
         <button className={classes.deleteButton} onClick={() => props.onTextChanges( '⌫')}>Backspace</button>
         </div>
+
 
       
       </div>
